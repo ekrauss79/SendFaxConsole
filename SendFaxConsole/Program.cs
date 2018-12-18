4608c318d8cf67781425970071e856c360789a8a
Eric Krauss <eric@jozuforwomen.com>
2018-12-17 17:42:31 -0800

    comming change

--- SendFaxConsole/Program.cs
+++ SendFaxConsole/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using InterFAX.Api;
 
@@ -11,33 +14,79 @@ namespace SendFaxConsole
     {
         public static void Main(string[] args)
         {
-            SendFaxAsync().Wait();
+            SendFaxAsync2().Wait();
         }
 
         static private async Task SendFaxAsync()
         {
 
             var interfax = new FaxClient(username: "ekrauss", password: "V2shC2t1!");
-            int returnVal = 0;
 
             try
             {
                 var options = new SendOptions { FaxNumber = "+18187015973" };
 
                 // with a path
-                var faxDocument = interfax.Documents.BuildFaxDocument(@".\FaxDocs\fax.txt");
-                var messageId = await interfax.Outbound.SendFax(faxDocument, options);
-                returnVal = 1;
+                //var fileDocument = interfax.Documents.BuildFaxDocument(@".\FaxDocs\fax.txt");
+                //var messageId = await interfax.Outbound.SendFax(faxDocument, options);
+                //returnVal = 1;
 
             }
             catch (Exception ex)
             {
-                returnVal = 0;
+                //returnVal = 0;
                 Console.WriteLine(ex.Message);
             }
 
         }
 
+        static private async Task SendFaxAsync2()
+        {
 
-    }
+            var interfax = new FaxClient(username: "ekrauss", password: "V2shC2t1!");
+            string myFilePath = Path.GetFullPath(@"C:\Users\ekrau\source\repos\SendFaxConsole\SendFaxConsole\FaxDocs\fax.txt").Replace(@"\\", @"\");
+
+            try
+            {
+                var faxId = await interfax.Outbound.SendFax(
+                  interfax.Documents.BuildFaxDocument(myFilePath),
+                  new SendOptions
+                  {
+                      FaxNumber = "+18187015973"
+                  }
+                );
+
+                // wait for the fax to be
+                // delivered successfully
+                while (true)
+                {
+                    // load the fax's status
+                    var fax = await interfax.Outbound.GetFaxRecord(faxId);
+                    // sleep if pending
+                    if (fax.Status < 0)
+                    {
+                        Thread.Sleep(10000);
+                    }
+                    else if (fax.Status == 0)
+                    {
+                        Debug.WriteLine("Sent!");
+                        break;
+                    }
+                    else
+                    {
+                        Debug.WriteLine($"Error: {fax.Status}");
+                        break;
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+        }
 }
